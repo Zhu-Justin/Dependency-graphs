90937b75fb4f1c0f42039e71cb9c2e575fcdf1bf
Justin Zhu <justinthezhu@gmail.com>
2020-09-11 12:10:37 -0400

    Saved new changes 2020-09-11 12:10:37

--- demo.py
+++ demo.py
@@ -4,16 +4,27 @@
 
 import os
 import argparse
-parser = argparse.ArgumentParser(description='Get Dependency Graphs')
-parser.add_argument('filename')
+from tree_sitter import Language, Parser
+
+PY_LANGUAGE = Language('build/my-languages.so', 'python')
+
+argparser = argparse.ArgumentParser(description='Get Dependency Graphs')
+argparser.add_argument('filename')
 # os.path.dirname()
 
-args = parser.parse_args()
+args = argparser.parse_args()
 print(args.filename)
 with open(args.filename, "r") as f:
     code = f.read() 
 
-print(code)
+parser = Parser()
+parser.set_language(PY_LANGUAGE)
+tree = parser.parse(bytes(code, "utf8"))
+root_node = tree.root_node
+
+# print(code)
+print(root_node.sexp())
+
 
 # parser.add_argument('integers', metavar='N', type=int, nargs='+',
 #                     help='an integer for the accumulator')
